0c3f28a0515f364ee9188dc148bb6385c9d0eac5
Zeth <theology@gmail.com>
2025-10-01 18:22:37 +0100

    Update comment.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -54,7 +54,7 @@ def assert_prog(instrs):
     assert args[3] == "hello"  # LOAD_CONST 'hello'
     assert int(getattr(args[4], "value", args[4])) == 1  # CALL 1 (tolerate enum/int)
     assert is_unset(args[5])  # POP_TOP
-    assert args[6] is None  # RETURN_CONST None
+    assert args[6] is None  # LOAD_CONST None
 
 
 def test_parser_emits_expected_instrs(tmp_path: Path):
